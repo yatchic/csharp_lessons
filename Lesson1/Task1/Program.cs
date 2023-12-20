9bd299027abc08ad6563326eba597e149ca002a4
Ivan Ryzhkovich <yatchic132@gmail.com>
2023-12-20 21:56:23 +0300

    дз задача с 5 гирями наибольшие данные в конце

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,4 +1,25 @@
-﻿int a=7;
-int b=5;
-if(a>b){Console.WriteLine($"гиря a тяжелее={a}");}else{Console.WriteLine($"гиря b тяжелее={b}");}
- 
+﻿int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+String g="";
+int max = a;
+if (b > max)
+{
+    max = b;g="b";
+}
+if (c > max)
+{
+    max = c;g="c";
+}
+if (d > max)
+{
+    max = d;g="d";
+}
+if (e > max)
+{
+    max = e;g="e";
+}
+
+ Console.WriteLine($"самая тяжелая гиря:{g}={max}"); 
