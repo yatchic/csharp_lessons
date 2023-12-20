e5cc712e6617931dd619739ad26142bc23634b95
Ivan Ryzhkovich <yatchic132@gmail.com>
2023-12-20 16:45:25 +0300

    add some variables

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello World");
+﻿//Console.WriteLine("Hello World");
+int value=100;
+Console.WriteLine(value);
+double d=5.55;
+char c='!';
+bool b=true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
