1917cc954a699fa9ac4c091068393985af3743a5
Ivan Ryzhkovich <yatchic132@gmail.com>
2023-12-21 00:23:17 +0300

    дз вывод чисел от 1 до N

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,25 +1,3 @@
-﻿int a = 7;
-int b = 10;
-int c = 6;
-int d = 4;
-int e = 9;
-String g="";
-int max = a;
-if (b > max)
-{
-    max = b;g="b";
-}
-if (c > max)
-{
-    max = c;g="c";
-}
-if (d > max)
-{
-    max = d;g="d";
-}
-if (e > max)
-{
-    max = e;g="e";
-}
-
- Console.WriteLine($"самая тяжелая гиря:{g}={max}"); 
+﻿int n=100;
+int i=1;
+while(i<=n){ Console.WriteLine(i);i++;}
